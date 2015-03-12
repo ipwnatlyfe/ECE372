46f66c89815094d70f8ba23dbf92de74877f683f
ipwnatlyfe <swirth@email.arizona.edu>
2015-03-12 02:52:15 -0700

    A few updates

--- Labs/lab2p1.X/lab2p1.c
+++ Labs/lab2p1.X/lab2p1.c
@@ -18,11 +18,24 @@ _CONFIG1( JTAGEN_OFF & GCP_OFF & GWRP_OFF & BKBUG_ON & COE_OFF & ICS_PGx1 &
 _CONFIG2( IESO_OFF & SOSCSEL_SOSC & WUTSEL_LEG & FNOSC_PRIPLL & FCKSM_CSDCMD & OSCIOFNC_OFF &
           IOL1WAY_OFF & I2C1SEL_PRI & POSCMOD_XT )
 
+typedef enum stateTypeENUM{
+    WAIT, START
+} stateType;
+
+volatile stateType currstate;
+initLCD();
+initKeypad();
+
+
 int main(void)
 {
-
+    currstate = WAIT;
     while(1){
         testLCD();
+        switch (currstate)
+        {
+
+        }
     }
     
     return 0;
@@ -30,5 +43,36 @@ int main(void)
 
 void _ISR _CNInterrupt(void){
     IFS1bits.CNIF = 0;
+    delayUs(5000);
+    int ButtonPushed = 0;
+
+    //Check to see that the button is still pushed meaning at least one of the inputs
+    //is low voltage
+    if((LATBbits.LATB7 == 0) || (LATBbits.LATB10 == 0) || (LATBbits.LATB11 == 0))
+    {
+        ButtonPushed = 1;
+    }
+    else
+    {
+        ButtonPushed = 0;
+    }
+
+    if(currstate == WAIT && ButtonPushed == 1)
+    {
+        currstate = START;
+    }
+    else if(currstate == WAIT && ButtonPushed == 0)
+    {
+        ButtonPushed = 0;
+    }
+    else if(currstate == START && ButtonPushed == 1)
+    {
+        int checkChar = scanKeypad();
+        if(checkChar != -1)
+        {
+            printCharLCD(checkChar);
+        }
+        currstate = WAIT;
+    }
     
 }
