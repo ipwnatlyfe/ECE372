592d4ffe4ea234abddf5e087ec8ea2803b09d4fd
ipwnatlyfe <swirth@email.arizona.edu>
2015-03-29 21:19:18 -0700

    Partially Working Part 1

--- Labs/lab3p1.X/lab3p1.c
+++ Labs/lab3p1.X/lab3p1.c
@@ -19,7 +19,7 @@ int main(void)
 {
 initADC();
 initLCD();
-//initPWM();
+initPWM();
 
 while(1)
 {
--- Labs/lab3p1.X/pwm.c
+++ Labs/lab3p1.X/pwm.c
@@ -2,14 +2,29 @@
 #include "pwm.h"
 void initPWM(){
 
-T2CONbits.TCKPS = 3;
-TMR2 = 0;
-PR2 = 56;
+TRISBbits.TRISB3 = 0;
+TRISBbits.TRISB9 = 0;
+LATBbits.LATB3 = 1;
+LATBbits.LATB9 = 1;
+
+T3CONbits.TCKPS = 3;
+TMR3 = 0;
+PR3 = 56;
 OC1CONbits.OCTSEL = 1; // using timer 3
 OC1CONbits.OCM = 6;
-RPOR5bits.RP11R = 18; // map OC1 to pin 22
+RPOR1bits.RP2R = 18; // map OC1 to pin 6
 OC1R = 0; //very important
 OC1RS = 14;
-T2CONbits.TON = 1;
+//T3CONbits.TON = 1;
+
+T3CONbits.TCKPS = 3;
+TMR3 = 0;
+PR3 = 56;
+OC2CONbits.OCTSEL = 1; // using timer 3
+OC2CONbits.OCM = 6;
+RPOR4bits.RP8R = 18; // map OC2 to pin 17
+OC2R = 0; //very important
+OC2RS = 14;
+T3CONbits.TON = 1;
 
 }
